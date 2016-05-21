c9108edc19e9155fa8c0143c501daed023147d2f
Joshua Sullivan <jsulliva@nerdery.com>
2016-05-21 11:35:17 -0500

    Setting up the Quiz screen.

--- TKD Korean Terms/ViewControllers/QuizViewController.swift
+++ TKD Korean Terms/ViewControllers/QuizViewController.swift
@@ -12,10 +12,25 @@ class QuizViewController: UIViewController {
     
     var beltLevel: BeltLevel!
     
+    @IBOutlet private weak var languageLabel: UILabel!
+    @IBOutlet private weak var termLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.navigationItem.title = beltLevel.name
-        print(allKoreanTerms)
+        
+        guard let categories = beltLevel.categories else {
+            languageLabel.hidden = true
+            termLabel.text = NSLocalizedString("This belt level has no terms.", comment: "Warning when a belt level has no terms.")
+            return
+        }
+        
+        let terms = categories.flatMap {
+            category in
+            return category.terms
+        }
+        
+        print(terms)
     }
 }
